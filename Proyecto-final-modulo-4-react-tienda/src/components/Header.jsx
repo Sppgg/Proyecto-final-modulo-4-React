fe8bcb5e06cada42b3920e4b93de47b837134625
Sppgg <saracodestheweb@gmail.com>
2025-03-16 20:46:01 +0000

    added logo to header

--- Proyecto-final-modulo-4-react-tienda/src/components/Header.jsx
+++ Proyecto-final-modulo-4-react-tienda/src/components/Header.jsx
@@ -6,20 +6,23 @@ import { Link } from "react-router-dom";
 import "../styles/HeaderComponent.css";
 import { AuthContext } from "../contexts/AuthContext";
 import { FaShoppingCart } from "react-icons/fa"; //importamos el icono del carrito
+import logo from "../assets/logo.png";
 
 const Header = () => {
   const { auth, logout } = useContext(AuthContext);
 
   return (
     <header className="header">
+      <div className="logo-container">
+        <Link to="/">
+          <img src={logo} alt="Smart Home Store Logo" className="logo" />
+        </Link>
+      </div>
       <nav>
         <ul className="nav-list">
           <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
             <Link to="/cart">
-              <FaShoppingCart />
+              <FaShoppingCart size={20} color="#333" />
             </Link>
           </li>
           {auth.token && (
